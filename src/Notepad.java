a880080807537efb293e357c44ec03cc34fae82c
filip.norberg <filipnorberg02@outlook.com>
2020-03-13 11:22:24 +0100

    klar v11

--- src/Notepad.java
+++ src/Notepad.java
@@ -1,2 +1,19 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class Notepad {
+
+    public static void main(String[] args) {
+
+
+
+        JPanel panel;
+        JButton button;
+        JMenuBar menu;
+        JMenu submenu;
+
+        Dimension d = new Dimension();
+        d.setSize(30, 30);
+
+    }
 }
